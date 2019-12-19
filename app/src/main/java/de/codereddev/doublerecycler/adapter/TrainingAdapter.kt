fb331f6d2dedda9770a3a61727e4684096fda31c
richardmac <fehlerrichard>
2019-12-19 18:31:44 +0100

    kommentare hinzugefügt zu Trainingadapter

--- app/src/main/java/de/codereddev/doublerecycler/adapter/TrainingAdapter.kt
+++ app/src/main/java/de/codereddev/doublerecycler/adapter/TrainingAdapter.kt
@@ -12,12 +12,16 @@ import de.codereddev.doublerecycler.model.Training
 
 class TrainingAdapter : RecyclerView.Adapter<TrainingAdapter.ViewHolder>() {
 
+
+    // interface fuer den add button auf jedem item in Rec View
     interface TrainingClickListener {
         fun onClick(uuid: String, editText: EditText, adapter: TrainingSetAdapter)
     }
 
+    // Liste der Trainings: elemente der obersten RV
     private val trainingList: MutableList<Training> = mutableListOf()
 
+    //TODO was ist das
     private val viewPool = RecyclerView.RecycledViewPool()
 
     private var listener: TrainingClickListener? = null
@@ -36,23 +40,32 @@ class TrainingAdapter : RecyclerView.Adapter<TrainingAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            // child RV inside parent RV
         trainingList[position].let { training ->
+
             val childManager = LinearLayoutManager(holder.recyclerView.context)
+            // Adapter Init  für jedes it==training mit it.sets
             val childAdapter = TrainingSetAdapter(training.sets)
 
             holder.recyclerView.apply {
                 layoutManager = childManager
                 adapter = childAdapter
+                // TODO was macht das setRec..
                 setRecycledViewPool(viewPool)
             }
 
             holder.addBtn.setOnClickListener {
+                // call interface fun onClick von TrainingClickListener
+                // uebergibt die UUID den edittext und den childRV Adapter des aktuellen training elements
                 listener?.onClick(training.uuid, holder.editText, childAdapter)
             }
         }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        /**@see onBindViewHolder
+         * hier ist der Trick: der ViewHolder des Parent RecView hält eine child RV
+        */
         val recyclerView: RecyclerView = itemView.findViewById(R.id.innerRv)
         val editText: EditText = itemView.findViewById(R.id.editText)
         val addBtn: Button = itemView.findViewById(R.id.addSetBtn)
