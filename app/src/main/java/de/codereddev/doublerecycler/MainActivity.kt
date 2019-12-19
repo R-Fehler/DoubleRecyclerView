1f559e484d9e082a08e44aaf80079aecde179ab8
richardmac <fehlerrichard>
2019-12-19 18:49:51 +0100

    kommentare hinzugefügt zu MainActivity

--- app/src/main/java/de/codereddev/doublerecycler/MainActivity.kt
+++ app/src/main/java/de/codereddev/doublerecycler/MainActivity.kt
@@ -19,48 +19,63 @@ class MainActivity : AppCompatActivity(), TrainingAdapter.TrainingClickListener 
     private var recyclerView: RecyclerView? = null
     private val parentAdapter = TrainingAdapter()
 
+    // implementiere die onClick Interface fun von TrainingAdapter
     override fun onClick(uuid: String, editText: EditText, adapter: TrainingSetAdapter) {
         if (editText.text.isEmpty())
             return
 
         editText.text.let {
-            val set = addSetToTraining(uuid, it.toString())
+            // wenn text nicht leer ist adde ein Set zu Training Objekt mit name == it
+            val set = addSetToTraining(uuid, it.toString()) //returns a TrainingSet nachdem dieses im Realm
+            // im passenden Training des Realms gespeichert wurde
             editText.text.clear()
-            adapter.addTrainingSet(set)
+            adapter.addTrainingSet(set) // füge es der Liste des Adapters hinzu und notify das es geändert ist
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // übergebe den einzelnen parent RV items den in der MainAct. impl. onClick
+        // this ist hier die MainAct. die TrainingAdapter.TrainingClickListener ja extended bzw implementiert
         parentAdapter.setOnClickListener(this)
 
+
+        // hier wird der Parent RV Adapter zugewiesen (nachdem der onClickListener übergeben /gesetzt wurde
         recyclerView = findViewById<RecyclerView>(R.id.outterRv).apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = parentAdapter
         }
 
+        // hier wird der FAB rechts unten initialisiert
         findViewById<FloatingActionButton>(R.id.addFab).apply {
+            // soll ein Training adden und die View updaten
             setOnClickListener {
-                addTraining()
+                addTraining() //erstellt ein neues Realm Training Objekt. nicht in Memory vom Adapter!
                 reloadData()
             }
         }
-
+        // das erste mal wurde ja kein knopf gedrückt also wird hier das erste mal die View upgedated
+        // mit Daten aus der Realm DB
         reloadData()
     }
 
     fun addSetToTraining(uuid: String, text: String): TrainingSet {
         DoubleRecyclerRealm.newRealmInstance().use {
             val set = TrainingSet().apply {
-                this.text = text
+                this.text = text // init den Set mit dem namen text
             }
 
             it.executeTransaction { realm ->
-                val managedSet = realm.copyToRealm(set)
+                val managedSet = realm.copyToRealm(set) // kopiert den Set in die Realm Instanz
+                // wo das Training diesselbe ID hat wird ein
+                // Realm.Query zurück gegeben (= hier vom Typ Training)
+                // wird der Set eingefügt (somit korrektes Datum etc)
                 val training: Training? = realm.where(Training::class.java).equalTo("uuid", uuid).findFirst()
+                // hier wird dem Training aus dem realm der Satz hinzugefügt
                 training?.sets?.add(managedSet)
             }
+            // gebe set mit dem namen initialisiert zurück
             return set
         }
     }
@@ -91,7 +106,11 @@ class MainActivity : AppCompatActivity(), TrainingAdapter.TrainingClickListener 
 
     fun reloadData() {
         DoubleRecyclerRealm.newRealmInstance().use {
+            // sucht alle Training Objekte in Realm un gibt sie an den parentAdapter zurück
             val result = it.where(Training::class.java).findAll()
+            /*copyFromRealm Makes an unmanaged in-memory copy of already persisted RealmObjects. This is a deep copy that will copy all referenced objects.
+            //The copied objects are all detached from Realm and they will no longer be automatically updated.
+            */
             parentAdapter.swapData(it.copyFromRealm(result))
         }
     }
